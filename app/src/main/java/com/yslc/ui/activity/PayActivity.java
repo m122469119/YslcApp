7a15fe9e7ec4c047ac4f0307320da4ebd18fc16a
tommy <zkwugz@163.com>
2016-06-01 14:37:18 +0800

    PayActivity 写了一些为支付宝准备的代码

--- app/src/main/java/com/yslc/ui/activity/PayActivity.java
+++ app/src/main/java/com/yslc/ui/activity/PayActivity.java
@@ -11,6 +11,7 @@ import android.widget.RadioButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.alipay.sdk.app.PayTask;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -25,17 +26,20 @@ import com.yslc.util.HttpUtil;
 import com.yslc.util.Md5Util;
 import com.yslc.util.ParseUtil;
 import com.yslc.util.SharedPreferencesUtil;
+import com.yslc.util.SignUtils;
 import com.yslc.util.ToastUtil;
-import com.yslc.wxapi.WXPayEntryActivity;
 
 
 import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.LinkedList;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PayActivity extends AppCompatActivity {
     private GoodBean product;
@@ -47,11 +51,19 @@ public class PayActivity extends AppCompatActivity {
 
     private static String orderPrice;
 
+    //------微信支付-----------
     public static final String APPID = "wx0955e887ac142b61";
 
     private static final String PARTNER_KEY = "44934854899E40719F17DA83BE8FEE00";
 
     private static final String PARTNET_ID = "1336494601";
+    //------微信支付----------
+    //------支付宝支付--------
+    private static final String PARTNER = "";//商户 ID
+    private static final String SELLER = "";//商户收款账号
+    private static final String RSA_PRIVATE = "";//商户私钥
+    private static final String RSA_PUBLIC = "";//支付宝公钥
+    //------支付宝支付--------
 
     private Button btn_pay;
     private RadioButton wechat_radio, alipay_radio;
@@ -60,11 +72,13 @@ public class PayActivity extends AppCompatActivity {
 
     /**
      * 获取订单号
+     *
      * @return
      */
     public static String getOrderNo() {
         return orderNo;
     }
+
     public static String getOrderPrice() {
         return orderPrice;
     }
@@ -90,18 +104,19 @@ public class PayActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == LOGIN_REQUEST_CODE && resultCode == RESULT_OK){
+        if (requestCode == LOGIN_REQUEST_CODE && resultCode == RESULT_OK) {
             isVip();
         }
     }
 
     private static final int LOGIN_REQUEST_CODE = 1;
+
     private void isVip() {
         //判断用户是否登录
         if (!SharedPreferencesUtil.isLogin(this)) {
             ToastUtil.showMessage(this, "请先登录");
             this.startActivityForResult(new Intent(this, LoginActivity.class), LOGIN_REQUEST_CODE);
-            return ;
+            return;
         }
         //判读是否vip
         SharedPreferencesUtil share = new SharedPreferencesUtil(this, Constant.SPF_USER_INFO_NAME);
@@ -154,7 +169,7 @@ public class PayActivity extends AppCompatActivity {
         tvPayPrice = (TextView) findViewById(R.id.pay_price);
         tvTitle = (TextView) findViewById(R.id.title_pay);
         //选择微信支付/支付宝
-        wechat_radio =(RadioButton)findViewById(R.id.rbt_wechat);
+        wechat_radio = (RadioButton) findViewById(R.id.rbt_wechat);
         alipay_radio = (RadioButton) findViewById(R.id.rbt_alipay);
         r_wechat = (RelativeLayout) findViewById(R.id.wechat_pay);
         r_wechat.setOnClickListener(new View.OnClickListener() {
@@ -184,36 +199,193 @@ public class PayActivity extends AppCompatActivity {
             public void onClick(View view) {
 //                startActivity(new Intent(PayActivity.this, WXPayEntryActivity.class));
                 //发起支付
-                if(btn_pay.getText().equals("返回")){
+                if (btn_pay.getText().equals("返回")) {
                     finish();
-                }else if (wechat_radio.isChecked()) {
-                    if(isHaveWeChat()){
+                } else if (wechat_radio.isChecked()) {
+                    if (isHaveWeChat()) {
                         payVip();
                     }
                 } else if (alipay_radio.isChecked()) {
                     //TODO 支付宝支付
+                    alipay();
                 }
             }
         });
-        if(getIntent().getStringExtra("activity") != null &&
-                getIntent().getStringExtra("activity").equals("payFailure")){//支付失败跳过来
+        if (getIntent().getStringExtra("activity") != null &&
+                getIntent().getStringExtra("activity").equals("payFailure")) {//支付失败跳过来
             tvTitle.setText("支付失败，请重新支付");
-        }else {
+        } else {
             tvTitle.setText(getResources().getString(R.string.pay_title));
         }
     }
 
     /**
+     * 支付宝支付
+     */
+    private void alipay() {
+        //拼接订单信息
+        String orderInfo = getOrderInfo(product.getProductId(), product.getProductName(),
+                product.getPrice());
+
+        String sign = sign(orderInfo);//对订单签名
+
+        //对sign 做URL编码
+        try {
+            sign = URLEncoder.encode(sign, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * 完整的符合支付宝参数规范的订单信息
+         * <p>商品信息加上签名生成订单信息</p>
+         */
+        final String payInfo = orderInfo + "&sign=\"" +
+                sign + "\"&" + getSignType();
+
+        //异步发起支付
+        Runnable payRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // 构造PayTask 对象
+                PayTask alipay = new PayTask(PayActivity.this);
+                //调用支付接口，获取支付结果
+                String result = alipay.pay(payInfo, true);//true代码使用loading
+
+                //TODO 处理支付结果 参数result
+//                /**
+//                 * 同步返回的结果必须放置到服务端进行验证（验证的规则请看https://doc.open.alipay.com/doc2/
+//                 * detail.htm?spm=0.0.0.0.xdvAU6&treeId=59&articleId=103665&
+//                 * docType=1) 建议商户依赖异步通知
+//                 */
+//                String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+//
+//                String resultStatus = payResult.getResultStatus();
+//                // 判断resultStatus 为“9000”则代表支付成功，具体状态码代表含义可参考接口文档
+//                if (TextUtils.equals(resultStatus, "9000")) {
+//                    Toast.makeText(PayDemoActivity.this, "支付成功", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    // 判断resultStatus 为非"9000"则代表可能支付失败
+//                    // "8000"代表支付结果因为支付渠道原因或者系统原因还在等待支付结果确认，最终交易是否成功以服务端异步通知为准（小概率状态）
+//                    if (TextUtils.equals(resultStatus, "8000")) {
+//                        Toast.makeText(PayDemoActivity.this, "支付结果确认中", Toast.LENGTH_SHORT).show();
+//
+//                    } else {
+//                        // 其他值就可以判断为支付失败，包括用户主动取消支付，或者系统返回的错误
+//                        Toast.makeText(PayDemoActivity.this, "支付失败", Toast.LENGTH_SHORT).show();
+//
+//                    }
+//                }
+            }
+        };
+
+        //必须异步调用
+        Thread payThread = new Thread(payRunnable);
+        payThread.start();
+    }
+
+    /**
+     * get the sign type we use. 获取签名方式
+     */
+    private String getSignType() {
+        return "sign_type=\"RSA\"";
+    }
+
+    /**
+     * sign the order info. 对订单信息进行签名
+     *
+     * @param content 待签名订单信息
+     */
+    private String sign(String content) {
+        return SignUtils.sign(content, RSA_PRIVATE);
+    }
+
+    /**
+     * 支付宝创建点单信息
+     *
+     * @param productId
+     * @param productName
+     * @param price
+     * @return
+     */
+    private String getOrderInfo(String productId, String productName, String price) {
+        //TODO 拼接订单
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int i = 0; i < params.size(); i++) {
+//            sb.append(params.get(i).getName());
+//            sb.append('=');
+//            sb.append(params.get(i).getValue());
+//            sb.append('&');
+//        }
+        //签约合作者身份ID
+        String orderInfo = "partner=" + "\"" + PARTNER + "\"";
+        //签约卖家支付宝账号
+        orderInfo += "&seller_id=" + "\"" + SELLER + "\"";
+        //商户网站唯一订单号
+        orderInfo += "&out_trade_no=" + "\"" + getOutTradeNo() + "\"";
+        //商品名称
+        orderInfo += "&subject=" + "\"" + productId + "\"";
+        //商品详情
+        orderInfo += "&body=" + "\"" + productName + "\"";
+        //商品金额
+        orderInfo += "&total_fee=" + "\"" + price + "\"";
+        //TODO 待改服务器异步通知页面路径
+        orderInfo += "&notify_url=" + "\"" + "http://notify.msp.hk/notify.htm" + "\"";
+        //服务接口名称， 固定值
+        orderInfo += "&service=" + "\"" + "mobile.securitypay.pay" + "\"";
+        //支付类型， 固定值
+        orderInfo += "&payment_type=" + "\"" + 1 + "\"";
+        //参数编码， 固定值
+        orderInfo += "&_input_charset=" + "\"" + "utf-8" + "\"";
+
+        // 设置未付款交易的超时时间
+        // 默认30分钟，一旦超时，该笔交易就会自动被关闭。
+        // 取值范围：1m～15d。
+        // m-分钟，h-小时，d-天，1c-当天（无论交易何时创建，都在0点关闭）。
+        // 该参数数值不接受小数点，如1.5h，可转换为90m。
+        orderInfo += "&it_b_pay=\"30m\"";
+
+        // extern_token为经过快登授权获取到的alipay_open_id,带上此参数用户将使用授权的账户进行支付
+        // orderInfo += "&extern_token=" + "\"" + extern_token + "\"";
+
+//        //支付宝处理完请求后，当前页面跳转到商户指定页面的路径，可空
+//        orderInfo += "&return_url=\"m.alipay.com\"";
+
+        // 调用银行卡支付，需配置此参数，参与签名， 固定值 （需要签约《无线银行卡快捷支付》才能使用）
+        // orderInfo += "&paymethod=\"expressGateway\"";
+        return orderInfo;
+    }
+
+    /**
+     * 支付宝支付 获取唯一订单号
+     *
+     * @return
+     */
+    private String getOutTradeNo() {
+//        SimpleDateFormat format = new SimpleDateFormat("MMddHHmmss", Locale.getDefault());
+//        Date date = new Date();
+//        String key = format.format(date);
+//
+//        Random r = new Random();
+//        key = key + r.nextInt();
+//        key = key.substring(0, 15);
+//        return key;
+        return null;
+    }
+
+    /**
      * 判断是否安装微信
+     *
      * @return
      */
     private boolean isHaveWeChat() {
-        if( !api.isWXAppInstalled()){
+        if (!api.isWXAppInstalled()) {
             ToastUtil.showMessage(PayActivity.this, "您未安装微信，请先下载微信客户端");
             return false;
-        }else if(api.getWXAppSupportAPI() >= Build.PAY_SUPPORTED_SDK_INT){
+        } else if (api.getWXAppSupportAPI() >= Build.PAY_SUPPORTED_SDK_INT) {
             return true;
-        }else {
+        } else {
             ToastUtil.showMessage(PayActivity.this, "您的微信版本太低，不支持支付，请更新");
             return false;
         }
@@ -236,7 +408,7 @@ public class PayActivity extends AppCompatActivity {
     }
 
     /**
-     * 发起支付
+     * 发起微信支付
      */
     private void payVip() {
         SharedPreferencesUtil share = new SharedPreferencesUtil(this, Constant.SPF_USER_INFO_NAME);
@@ -260,13 +432,14 @@ public class PayActivity extends AppCompatActivity {
 
     /**
      * 开启微信客户端支付
+     *
      * @param s
      */
     private void startWeiXin(String s) {
 
-        try{
-            JSONObject object =new JSONObject(s);
-            if(null != object ){
+        try {
+            JSONObject object = new JSONObject(s);
+            if (null != object) {
                 PayReq req = new PayReq();
                 req.appId = APPID;
                 req.partnerId = PARTNET_ID;
@@ -277,25 +450,46 @@ public class PayActivity extends AppCompatActivity {
                 orderNo = object.getString("orderNo");
 //                req.packageValue = "Sign=WXPay";
 
-                List<NameValuePair> signParams = new LinkedList<NameValuePair>();
-                signParams.add(new BasicNameValuePair("appid", req.appId));
-                signParams.add(new BasicNameValuePair("noncestr", req.nonceStr));
-                signParams.add(new BasicNameValuePair("package", req.packageValue));
-                signParams.add(new BasicNameValuePair("partnerid", req.partnerId));
-                signParams.add(new BasicNameValuePair("prepayid", req.prepayId));
-                signParams.add(new BasicNameValuePair("timestamp", req.timeStamp));
-                req.sign = genAppSign(signParams);
+                LinkedHashMap<String ,String> params = new LinkedHashMap<String,String>();
+                params.put("appid", req.appId);
+                params.put("noncestr", req.nonceStr);
+                params.put("package", req.packageValue);
+                params.put("partnerid", req.partnerId);
+                params.put("prepayid", req.prepayId);
+                params.put("timestamp", req.timeStamp);
+                req.sign = genAppSign2(params);
+
 //                req.extData = "app data";//optional
                 api.sendReq(req);
                 finish();
             }
-        }catch (JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
+    private String genAppSign2(LinkedHashMap<String, String> params) {
+        StringBuilder sb = new StringBuilder();
+
+        Iterator itrt =params.entrySet().iterator();
+        while(itrt.hasNext()){
+            Map.Entry entry = (Map.Entry) itrt.next();
+            sb.append(entry.getKey());
+            sb.append('=');
+            sb.append(entry.getValue());
+            sb.append('&');
+        }
+
+        sb.append("key=");
+        sb.append(PARTNER_KEY);
+
+        String appSign = Md5Util.getMD5(sb.toString().getBytes()).toUpperCase();
+        return appSign;
+    }
+
     /**
      * 使用MD5生成sign参数
+     *
      * @param params
      * @return
      */
@@ -312,7 +506,7 @@ public class PayActivity extends AppCompatActivity {
         sb.append(PARTNER_KEY);
 
         String appSign = Md5Util.getMD5(sb.toString().getBytes()).toUpperCase();
-        return  appSign;
+        return appSign;
 //        return URLEncodedUtils.format(params,"utf_8") + "&sign="+appSign;
     }
 
@@ -323,13 +517,13 @@ public class PayActivity extends AppCompatActivity {
         RequestParams params = new RequestParams();
         params.put("ProductId", "YSLC0001");
         HttpUtil.originGet(HttpUtil.GET_PRODUCT, this, params,
-                new JsonHttpResponseHandler(){
+                new JsonHttpResponseHandler() {
                     @Override
                     public void onSuccess(int i, JSONObject jsonObject) {
                         super.onSuccess(i, jsonObject);
-                        if(jsonObject.optInt("status")== 0){//失败
+                        if (jsonObject.optInt("status") == 0) {//失败
                             ToastUtil.showMessage(PayActivity.this, jsonObject.optString("msg"));
-                        }else if(jsonObject.optInt("status")== 1) { //成功
+                        } else if (jsonObject.optInt("status") == 1) { //成功
                             product = ParseUtil.parseGoodBean(jsonObject);
                             showData();
                         }
@@ -348,6 +542,4 @@ public class PayActivity extends AppCompatActivity {
         tvPayPrice.setText(product.getPrice());
         tvPayItem.setText(product.getProductName());
     }
-
-
 }
